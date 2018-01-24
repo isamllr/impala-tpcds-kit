fdddf2be91f1833569ffef120b3cc2402c032310
isamllr <isamllr@users.noreply.github.com>
2018-01-24 19:25:28 +0100

    Update tpcds-env.sh

--- tpcds-env.sh
+++ tpcds-env.sh
@@ -2,11 +2,11 @@
 export TPCDS_ROOT=$HOME/tpcds-kit
 
 # top level directory for flat files in HDFS
-export FLATFILE_HDFS_ROOT=/user/${USER}/tpcds
+#export FLATFILE_HDFS_ROOT=/user/${USER}/tpcds
 #Adjust to use Azure Data Lake
 #export FLATFILE_HDFS_ROOT=adl://adlane.azuredatalakestore.net/${USER}/tpcds
 #Use external blob storage according to https://blogs.msdn.microsoft.com/pliu/2016/06/19/backup-cloudera-data-to-azure-storage/
-#export FLATFILE_HDFS_ROOT=wasb://cloudera@limitlesstestaccount1.blob.core.windows.net/${USER}/tpcds
+export FLATFILE_HDFS_ROOT=wasb://cloudera@limitlesstestaccount1.blob.core.windows.net/${USER}/tpcds/
 
 # scale factor in GB
 # SF 3000 yields ~1TB for the store_sales table
@@ -21,7 +21,7 @@ export DSDGEN_NODES=18
 #export DSDGEN_THREADS_PER_NODE=19
 # For DS14v2 with 16 cores: Use 15 threads, one left for machine
 #Standard_D32s_v3 has 32 vCPUs, leave one
-export DSDGEN_THREADS_PER_NODE=31
+export DSDGEN_THREADS_PER_NODE=19
 export DSDGEN_TOTAL_THREADS=$((DSDGEN_NODES * DSDGEN_THREADS_PER_NODE))
 
 # the name for the tpcds database
